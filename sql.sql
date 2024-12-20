8a3c595f82919fe8bb9891c8d20c499a451988e8
Minsu Yoo <minsoo0515@naver.com>
2024-12-20 20:41:43 +0900

    feat : Member and Post table

--- sql.sql
+++ sql.sql
@@ -1,10 +1,23 @@
 use
 
-CREATE TABLE post
-(
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    title      TEXT         NOT NULL,
-    content    LONGTEXT     NOT NULL,
-    thumbnail  VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP    NOT NULL,
+CREATE TABLE member (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE, -- 사용자 ID
+    password VARCHAR(255) NOT NULL,       -- 암호화된 비밀번호
+    role VARCHAR(20) NOT NULL,            -- 권한 (e.g., 'ROLE_ADMIN')
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title TEXT NOT NULL,
+    content LONGTEXT NOT NULL,
+    thumbnail VARCHAR(512) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    member_id INT NOT NULL, -- 작성자 ID (외래키)
+    FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE
+);
+
+-- 관리자 계정 추가
+INSERT INTO member (username, password, role)
+VALUES ('kchcompany', '$2a$10$6KlMPEe0Rb5kD/BKN0g6CuvJOGEeqY1aXB/7pH9ZDyTihAo5yFZdy', 'ROLE_ADMIN');
