f2d2d655517b3e78a87898c1842872d11fa644d0
Minsu Yoo <minsoo0515@naver.com>
2025-02-08 19:25:40 +0900

    feat : 컨트롤러 추가

---
+++ src/main/java/inquary/InquiryController.java
@@ -0,0 +1,22 @@
+package inquary;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/inquiry")
+public class InquiryController {
+
+    private final InquiryService inquiryService;
+
+    public InquiryController(InquiryService inquiryService) {
+        this.inquiryService = inquiryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Inquiry> createInquiry(@Valid @RequestBody Inquiry inquiry) {
+        Inquiry savedInquiry = inquiryService.saveInquiry(inquiry);
+        return ResponseEntity.ok(savedInquiry);
+    }
+}
